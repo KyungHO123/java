ab399fed80ce743bc51d69ff292ddf1a224e1e0f
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-08 17:14:39 +0900

    자바 1일차 :문자열 String 예제

---
+++ java_kh/src/day01/veriableEX5.java
@@ -0,0 +1,35 @@
+package day01;
+
+public class veriableEX5 
+{
+		//String 선언 예제
+	public static void main(String[] args)
+	{  //String은 무조건 대문자로 시작
+		String str1 = null;
+		System.out.println(str1);
+		
+		//기본형 변수에는 null을 저장할 수 없다.
+		//int num1 = null;
+		
+		String str2 = "Hello";
+		System.out.println(str2);
+		
+		//String 클래스에 한 문자를 저장할 수 없다.
+		//String str3 = 'A'
+		String str4 = "A";
+		System.out.println(str4);
+		
+		//String 클래스에 빈 문자열을 저장할 수 있다.
+		String str5 = "";
+		System.out.println(str5);
+		
+		//문자열에 정수 또는 실수 또는 문자 또는 논리값을 더하면 문자열이 됨
+		//빈 문자열 + 'A'는 "A"가 되어서 저장이 가능
+		String str6 = "" + 'A';
+		System.out.println(str6);
+		
+		
+		
+	}
+
+}
