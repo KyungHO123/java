5df8e324e5aab50ae999078946b762848930a0a1
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-08 14:24:39 +0900

    자바 1일차 : 샘플 코드 및 설명

---
+++ java_kh/src/day01/HelloWorld.java
@@ -0,0 +1,24 @@
+package day01;
+
+//한 줄 주석
+/* 여러줄 주석
+ * 
+ *  
+ */
+public class HelloWorld {    //주석
+	//주석
+	/**
+	 * 
+	 * 메소드를 설명할 때 사용
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("Hello World!");
+		//sysout은 콘솔에 출력할 때 사용
+		
+		
+   	}
+
+}
