5829c8b76d50832628e437d07d27d7f60ca5c57a
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-08 17:42:24 +0900

    자바 1일차: 문제

---
+++ java_kh/src/day01/TestEx1.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class TestEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1.아래 코드에서 잘못된 부분을 찾아 수정하세요.
+		//char ch1 = "A";
+		char ch1 = 'A';
+	}
+
+}
