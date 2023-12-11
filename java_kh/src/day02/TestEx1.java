e7b973c7bee3e7b8e6e74ed6c7cde4eb4ce414a7
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-11 12:47:31 +0900

    자바 2일차 : 연산자 test

---
+++ java_kh/src/day02/TestEx1.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class TestEx1 {
+		//연산자 예제
+	public static void main(String[] args) {
+		/*다음 코드를 이용하여 국어, 영어, 수학 성적의 평균을 구하여 콘솔에 출력하는 코드를 작성하세요*/
+		int korscore = 100, engscore = 50, mathscore = 92;
+		double sum = korscore + engscore + mathscore;
+		System.out.println("세 과목의 총합 : " +sum);
+		System.out.println("세 과목의 평균 : "+ sum/3);
+		
+		
+	}
+
+}
