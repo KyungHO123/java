71528964a19f5155758ccaf65304259c2a9c3d70
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-12 17:27:31 +0900

    자바3일차:컨티뉴

---
+++ java_kh/src/day03/ContinueEx1.java
@@ -0,0 +1,26 @@
+package day03;
+
+public class ContinueEx1 {
+
+	public static void main(String[] args) {
+		//continue를 이용하여 짝수 출력 예제
+		//홀수이면 건너뜀(스킵). 짝수이면 출력
+		for(int i = 1; i<=10; i++) { //continue는 반복문과 함께 사용하지 않으면 반복문의 의미가 없어짐
+			if(i%2 != 0) {
+			continue;}
+			
+			System.out.println(i);
+	
+	
+	
+	
+	
+	
+	
+		}
+		
+	}
+	
+}
+
+
