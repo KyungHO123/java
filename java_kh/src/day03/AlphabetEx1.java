e8b48b6cef528373fbc58ba86ca251ea8189023d
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-12 17:27:08 +0900

    자바3일차:알파벳테스트

---
+++ java_kh/src/day03/AlphabetEx1.java
@@ -0,0 +1,26 @@
+package day03;
+
+public class AlphabetEx1 {
+
+	public static void main(String[] args) {
+		/* a부터 z까지 반복문을 이용하여 출력하는 코드를 작성하세요.
+		 * 반복회수 :
+		 * 규칙성  :
+		 */
+		
+		for(char ch = 'a'; ch <= 'z';++ch) {
+			System.out.println(ch);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
