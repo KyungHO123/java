5eacf711c9edacf7b2bd671cc63cdfef267e72d4
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-13 12:46:27 +0900

    자바4일차: 구구콘

---
+++ java_kh/src/day04/MultipleTableEx1.java
@@ -0,0 +1,33 @@
+package day04;
+
+public class MultipleTableEx1 {
+
+	public static void main(String[] args) {
+		/*구구단 2단부터 9단까지 출력하는 코드를 작성하세요.
+		 * 반복회수 :num은 2부터 9까지 1씩 증가
+		 * 규칙성 : num단을 출력
+		 * */
+		
+		//2단 출력하는 코드를 작성하라.
+		//int num= 2;
+		for(int num = 2 ;num <=9 ;num++ ) {
+			System.out.println(num + "단");
+		
+		for(int i= 1 ;i <=9 ;i++ ) {
+			System.out.println(num + "X" + i + "=" +(num*i));
+	} 
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	}}
