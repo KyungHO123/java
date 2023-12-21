e918a87c69c151c59fde4aa90e387ebdff478d0b
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-21 17:44:01 +0900

    자바 10일차 : 연단어 프로그램 수정본

--- java_kh/src/day10/word/Word.java
+++ java_kh/src/day10/word/Word.java
@@ -1,12 +1,14 @@
 package day10.word;
 
+/*
+ * - Word 클래스
+ *   - word : 영단어
+ *   - meaning : 뜻
+ *   - 단어 출력, 단어 수정, 
+ *   - 단어 확인 : 주어진 문자열과 같은 단어인지 확인
+ */
 public class Word {
-	/* -Word 클래스
-	 *  -word : 영단어
-	 *  -meaning : 뜻 
-	 *  -단어 출력, 단어 수정
-	 *  -단어 확인 : 주어진 문자열과 같은 단어인지 확인.*/
-	
+
 	private String word;
 	private String meaning;
 	
@@ -16,6 +18,7 @@ public class Word {
 	public void setWord(String word) {
 		this.word = word;
 	}
+	
 	public String getMeaning() {
 		return meaning;
 	}
@@ -23,35 +26,28 @@ public class Word {
 		this.meaning = meaning;
 	}
 	
-	
-	
-
-	
-	
-	
-	/** 단어와 뜻을 출력하는 기능
-	 *  */
+	/**단어와 뜻을 출력하는 기능
+	 * */
 	public void print() {
 		System.out.println(word + " : " + meaning);
-		
 	}
+	
 	/**뜻이 주어지면 뜻을 수정하는 기능
 	 * @param meaning 수정할 뜻
-	 *  */
+	 */
 	public void update(String meaning) {
 		this.meaning = meaning;
-		}
+	}
 	/**단어가 주어지면 주어진 단어와 내 단어가 같은지 다른지를 알려주는 메서드
-	 * @param 잭ㅇ 같은지 다른지를 확인할 단어
-	 * @return 주어진 word와 내 word가 같으면 true,  다르면 false 리턴
+	 * @param word 같은지 다를지를 확인할 단어
+	 * @return 주어진 word와 내 word가 같으면 true, 다르면 false 리턴
 	 */
 	
 	public boolean equals(String word) {
 		return this.word.equals(word);
-		
 	}
 	//주어진 단어와 뜻으로 단어와 뜻을 초기화 하는 생성자
-	public Word(String Word,String meannig) {
+	public Word(String word, String meaning) {
 		this.word = word;
 		this.meaning = meaning;
 	}
