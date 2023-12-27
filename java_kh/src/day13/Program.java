7ae61260d56ad9fe7ee371b6b150567b3fbb3e23
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2023-12-27 17:32:19 +0900

    자바 13일차 :  롬복 프로그램 예제

---
+++ java_kh/src/day13/Program.java
@@ -0,0 +1,25 @@
+package day13;
+
+import lombok.Builder.Default;
+
+public interface Program {
+	
+	//int num1=10; //변수 앞에 final static이 자동으로 추가.
+	//final static int num2=20;
+	//메서드 앞에 public abstract가 자동으로 추가
+	
+	void printMenu();
+	public void runMenu(int menu);
+	public abstract void printExit();
+	void run();
+	default void test() {}
+}
+
+
+abstract class TestA{
+	//클래스에서는 final static을 생략하면 멤버변수, 붙여주면 상수
+	int num1=10;
+	final static int num2 = 20;
+	//클래스에서는 추상 메서드에 abstract가 자동으로 추가되지 않음
+	public abstract void printMenu();
+}
