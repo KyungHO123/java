567824d009fb6e1d4a6fe21ed9e08077162f9e60
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-01-03 17:09:16 +0900

    자바 4일차: 별예제

--- java_kh/src/day04/StarEx1.java
+++ java_kh/src/day04/StarEx1.java
@@ -6,27 +6,19 @@ public class StarEx1 {
 		/* *****
 		 * *****
 		 * *****
-		 * 이와 같이 출력이 되도록 작성해보세요.
-		 * 반복횟수 : i는 1부터 3까지 1씩 증가
-		 * 규칙성 : *을 5개 출력
-		 *        =>반복횟수 : j는 1부터 5까지 1씩증가
-		 *         규칙성 : *을 출력
+		 * 위와 같이 출력이 되도록 작성해보세요.
+		 * 반복횟수	: i는 1부터 3까지 1씩 증가
+		 * 규칙성		: *을 5개 출력
+		 * 			=> 	반복횟수	: j는 1부터 5까지 1씩 증가
+		 * 				규칙성	: *을 출력
+		 * 				반복문종료후: 엔터
 		 */
-		/*System.out.println("*****");
-		System.out.println("*****");
-		System.out.println("*****");*/
-		
-		/*String res = "*****";
-		for(int i =1;i <=3 ;i++) {
-			System.out.println(res);
-			
-		}	*/
-		//강사님 코드
-		for(int i =1;i<=3;i++) {
-			for(int j=1;;);
+		for(int i = 1; i<=3; i++) {
+			for(int j = 1; j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-		
-		
-		
 	}
+
 }
