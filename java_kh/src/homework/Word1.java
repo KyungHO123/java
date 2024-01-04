8e221a88be94aa61a5e06f54078fbec29007e492
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-01-04 16:09:45 +0900

    수정.

---
+++ java_kh/src/homework/Word1.java
@@ -0,0 +1,38 @@
+package homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import day15.student.Student;
+
+public class Word1 {
+	/*	영어 단어장을 관리하는 프로그램을 작성하세요.
+	 * 기한은 1/5까지 github에 업로드 후 강사에게 공유 (단톡방)
+	 * -한 단어에 뜻이 여러개 있을 수 있음
+	 * -단어, 품사, 뜻을 관리 
+	 * -단어 추가
+	 * -단어 수정
+	 * -단어 삭제
+	 * -뜻 추가
+	 * -뜻 수정
+	 * -뜻 삭제
+	 * -단어 조회
+	 * 
+	 * -기타 추가 기능을 구현해도 됨
+	 * -등록된 단어장에서 랜덤으로 문제가 나오고 맞추는 기능
+	 * -오답 노트를 관리하는 기능
+	 * -많이 조회한 단어를 확인하는 기능
+	 * */
+	private Scanner sc = new Scanner(System.in);
+	private final int EXIT = 8;
+	private List<Student> list = new ArrayList<Student>();
+	
+	public static void main(String[] args) {		
+		
+
+	}
+
+}
+
+		
