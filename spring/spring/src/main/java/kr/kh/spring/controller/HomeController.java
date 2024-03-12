3d42984858a15ec8b2da4bcd23b2e3af278f5805
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-03-12 16:24:20 +0900

    서블릿 1일차 : 스프링 세팅

--- spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.kh.spring.model.dto.TestDTO;
+import kr.kh.spring.model.vo.MemberVO;
 import kr.kh.spring.service.MemberService;
 
 @Controller
@@ -26,11 +27,10 @@ public class HomeController {
 	// Locale locale : 지역 정보
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-		//테스트용으로 등록된 회원 수를 조회
-		int count = memberService.testCountMember();
-		System.out.println("등록된 회원 수 : " + count);
+		MemberVO member = memberService.getMember("admin");
+		System.out.println(member);
 		// model.addAttribute("화면에서 사용할 이름", "보낼 데이터");
-		model.addAttribute("name", "홍길동");
+		model.addAttribute("name", "지드래곤");
 
 		return "home";
 	}
