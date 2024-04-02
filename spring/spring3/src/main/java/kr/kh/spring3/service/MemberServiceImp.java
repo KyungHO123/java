bead5a87bd7d644fd26045502c986333da0c3561
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2024-04-02 10:50:26 +0900

    스프링 복습2 : 404에러 수정 root-context url 수정 servlet-context url수정 , mapper 리절트타입 int로 수정

--- spring/spring3/src/main/java/kr/kh/spring3/service/MemberServiceImp.java
+++ spring/spring3/src/main/java/kr/kh/spring3/service/MemberServiceImp.java
@@ -2,14 +2,13 @@ package kr.kh.spring3.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import kr.kh.spring3.dao.MemberDAO;
 
 @Service
 public class MemberServiceImp implements MemberService {
 
 	@Autowired
-	MemberDAO memberDao;
+	private MemberDAO memberDao;
 
 	@Override
 	public int getMemberCount() {
